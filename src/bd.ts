42901dfb45f1fbf586b01dd45fbc174399cca601
Gabriel Costa <gabrielcostamgm@hotmail.com>
2025-09-25 23:07:05 -0300

    modify-bd-user                  git push origin main

--- src/bd.ts
+++ src/bd.ts
@@ -2,7 +2,7 @@ import { Post } from "../src/types/post";
 export const users = [
     { id: 1, name: "Thiago", email: "flamengodecoracao@gmail.com", senha: "flamengo123", age: 30, role: "admin" },
     { id: 2, name: "Gabriel Costa", email: "mgm@gmail.com", senha: "paulaodasolda123", age: 22, role: "user" },
-    { id: 3, name: "Maria Vitoria", email: "mavi@gmail.com", senha: "euaindaamominhaex", age: 19, role: "user" }
+    { id: 3, name: "Maria Vitoria", email: "mary@gmail.com", senha: "password", age: 19, role: "user" }
 ];
 
 export let posts : Post[] = [];
